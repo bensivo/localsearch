84245b8580e57119efaf01b3d453e7ecb6c2c096
Ben Sivoravong <bensivo@gmail.com>
2024-04-14 13:32:40 -0500

    e2e test suite

---
+++ packages/e2e/src/test_document_storage.py
@@ -0,0 +1,97 @@
+from faker import Faker
+import base64
+import uuid
+import grpc
+import localsearch_pb2
+import localsearch_pb2_grpc
+
+fake = Faker()
+
+def connect_grpc():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = localsearch_pb2_grpc.LocalsearchStub(channel)
+    return stub
+
+def generate_document():
+    document_id = fake.isbn10()
+    contents = fake.text()
+    contents_bytes = contents.encode("utf-8")
+    contents_base64 = base64.b64encode(contents_bytes).decode('utf-8')
+
+    return document_id, contents_base64
+
+
+def test_insert_document():
+    # Given we are connected to the GRPC server
+    stub = connect_grpc()
+    
+    # When we call InsertDocument()
+    request_id = str(uuid.uuid4())
+    document_id, contents_base64 = generate_document()
+
+    res = stub.InsertDocument(localsearch_pb2.InsertDocumentRequest(
+        request_id = request_id,
+        document_id = document_id,
+        contents_base64 = contents_base64
+    ))
+
+    # Then we get a success response
+    assert res.request_id == request_id
+    assert res.response_code == 0
+
+def test_get_document():
+    # Given we are connected to the GRPC server
+    stub = connect_grpc()
+
+    # Given a document has been inserted
+    document_id, contents_base64 = generate_document()
+    stub.InsertDocument(localsearch_pb2.InsertDocumentRequest(
+        request_id = str(uuid.uuid4()),
+        document_id = document_id,
+        contents_base64 = contents_base64
+    ))
+    
+    # When we call GetDocument()
+    request_id = str(uuid.uuid4())
+    res = stub.GetDocument(localsearch_pb2.GetDocumentRequest(
+        request_id = request_id,
+        document_id = document_id
+    ))
+
+    # Then we get the same document contents as we inserted
+    assert res.request_id == request_id
+    assert res.response_code == 0
+    assert res.contents_base64 == contents_base64
+
+
+
+def test_query():
+    # Given we are connected to the GRPC server
+    stub = connect_grpc()
+
+    # Given we have inserted a document
+    document_id, contents_base64 = generate_document()
+    stub.InsertDocument(localsearch_pb2.InsertDocumentRequest(
+        request_id = str(uuid.uuid4()),
+        document_id = document_id,
+        contents_base64 = contents_base64
+    ))
+
+    # When we make a query request
+    request_id = str(uuid.uuid4())
+    query = "hello"
+    req = localsearch_pb2.QueryRequest(
+        request_id = request_id,
+        query = query
+    )
+    res = stub.Query(req)
+
+    # Then that document is in the response, and has a score
+    assert res.request_id == req.request_id
+    assert res.response_code == 0
+    found = False
+    for ds in res.document_scores: 
+        if ds.document_id == document_id:
+            found = True
+            break
+    assert found == True
---
+++ packages/e2e/src/test_query.py
@@ -0,0 +1,61 @@
+from faker import Faker
+import base64
+import uuid
+import grpc
+import localsearch_pb2
+import localsearch_pb2_grpc
+
+fake = Faker()
+
+def connect_grpc():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = localsearch_pb2_grpc.LocalsearchStub(channel)
+    return stub
+
+def generate_document():
+    document_id = fake.isbn10()
+    contents = fake.text()
+
+    return document_id, contents
+
+def insert_document(stub, document_id, contents):
+    contents_bytes = contents.encode("utf-8")
+    contents_base64 = base64.b64encode(contents_bytes).decode('utf-8')
+
+    stub.InsertDocument(localsearch_pb2.InsertDocumentRequest(
+        request_id = str(uuid.uuid4()),
+        document_id = document_id,
+        contents_base64 = contents_base64,
+    ))
+
+def test_query():
+    # Given we're connected
+    stub = connect_grpc()
+
+    # Given we have 2 documents inserted
+    doc1_id = fake.isbn10()
+    doc1_contents = "Hello, world! It's a beautiful day today, isn't it?"
+
+    doc2_id = fake.isbn10()
+    doc2_contents = "Goodbye, cruel world. It's kind of a bummer today, isn't it?"
+
+    insert_document(stub, doc1_id, doc1_contents)
+    insert_document(stub, doc2_id, doc2_contents)
+
+    # When we query, with the text from document 1
+    res = stub.Query(localsearch_pb2.QueryRequest(
+        request_id = str(uuid.uuid4()),
+        query = "hello world beautiful day"
+    ))
+
+    # Then document 1 has a higher score than document 2
+    score_doc1 = 0
+    score_doc2 = 0
+
+    for ds in res.document_scores:
+        if ds.document_id == doc1_id:
+            score_doc1 = ds.score
+        if ds.document_id == doc2_id:
+            score_doc2 = ds.score
+    
+    assert score_doc1 > score_doc2
