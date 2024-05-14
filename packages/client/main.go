de6f36999a7cc4acd199ba5448c3aaa4acb60de3
Ben Sivoravong <bensivo@gmail.com>
2024-05-13 20:44:35 -0500

    WIP

--- packages/client/main.go
+++ packages/client/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"context"
+	"encoding/base64"
+	"fmt"
 	"html/template"
+	pb "localsearch-grpc/pkg/grpc"
 	"log"
 	"net/http"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Document struct {
@@ -13,6 +21,14 @@ type Document struct {
 
 func main() {
 	documents := []Document{}
+
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewLocalsearchClient(conn)
 
 	mux := http.NewServeMux()
 
@@ -59,7 +75,27 @@ func main() {
 		}
 		documents = append(documents, document)
 
-		// TODO: Submit document using GRPC service
+		id, err := uuid.NewRandom()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		res, err := client.InsertDocument(context.Background(), &pb.InsertDocumentRequest{
+			RequestId:      id.String(),
+			DocumentId:     document.Title,
+			ContentsBase64: base64.StdEncoding.EncodeToString([]byte(document.Content)),
+		})
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if res.ResponseCode != 0 {
+			http.Error(w, "Failed to insert document", http.StatusInternalServerError)
+			return
+		}
 
 		template, err := template.ParseFiles("./public/documents-insert-submit.html")
 		if err != nil {
@@ -75,7 +111,8 @@ func main() {
 		template.Execute(w, result)
 	})
 
-	err := http.ListenAndServe("localhost:8080", mux)
+	fmt.Println("Listening on http://localhost:8080")
+	err = http.ListenAndServe("localhost:8080", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
